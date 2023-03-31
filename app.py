7dcf4d2922738a848a8507c5ff0e551e8a7451c6
Wolt90 <gleb90@list.ru>
2023-04-01 00:27:47 +0300

    Delete README.md

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
-    # return render_template('index.html')	
+    # return 'Hello World'
+    return render_template('index.html')	
 
 
 if __name__ == '__main__':
